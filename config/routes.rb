0c6d9ebf80f8ac794a7f2e7df09079f02cdf9744
Francesca Santoriello <f.santoriello@hotmail.com>
2021-04-09 14:56:24 +0200

    dashboard boilerplate

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get '/dashboard', to: 'dashboard#index'
+  get '/dashboard/properties', to: 'dashboard#properties'
+  get '/dashboard/report', to: 'dashboard#report'
+
   resources :properties
   devise_for :accounts
 
