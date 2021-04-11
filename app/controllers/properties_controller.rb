466c21bee3d2abeb7d47d7933f6d8083e83e1409
Francesca Santoriello <f.santoriello@hotmail.com>
2021-04-11 14:41:05 +0200

    cloudinary setup

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -71,6 +71,6 @@ class PropertiesController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def property_params
-      params.require(:property).permit(:name, :address, :price, :rooms, :bedrooms, :bathrooms)
+      params.require(:property).permit(:name, :address, :price, :rooms, :bedrooms, :bathrooms, photos: [])
     end
 end
--- app/models/property.rb
+++ app/models/property.rb
@@ -1,3 +1,11 @@
 class Property < ApplicationRecord
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :price, presence: true
+  validates :rooms, presence: true
+  validates :bedrooms, presence: true
+  validates :bathrooms, presence: true
+
   belongs_to :account
+  has_many_attached :photo
 end
