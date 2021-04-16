2376f068b198d8784eeded2ec666d47f93873f39
Francesca Santoriello <f.santoriello@hotmail.com>
2021-04-16 19:21:51 +0200

    general style refine

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -1,10 +1,10 @@
 class PublicController < ApplicationController
   def main
-    @latest_properties = Property.latest[0..5]
+    @latest_properties = Property.latest[0..7]
     
-    if account_signed_in?
-      redirect_to dashboard_path, flash: { success: "Succesfully signed in. Welcome to Amsterdam Makelaars!" } and return
-    end
+    # if account_signed_in?
+    #   redirect_to dashboard_path, flash: { success: "Succesfully signed in. Welcome to Amsterdam Makelaars!" } and return
+    # end
   end
 
   def my_properties
