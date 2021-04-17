904cd84d969a7d59c67553343ccf9c307cc68378
Francesca Santoriello <f.santoriello@hotmail.com>
2021-04-17 18:41:52 +0200

    heroku debug

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 Rails.application.routes.draw do
   root to: 'public#main'
 
-  resources :properties
-  devise_for :accounts
+  # resources :properties
+  # devise_for :accounts
   
-  get '/properties/sale', to: 'properties#for_sale', as: 'for_sale'
-  get '/properties/rent', to: 'properties#for_rent', as: 'for_rent'
-  get '/account/properties', to: 'properties#my_properties'
-  post '/agent/message', to: 'properties#email_agent', as: 'email_agent'
+  # get '/properties/sale', to: 'properties#for_sale', as: 'for_sale'
+  # get '/properties/rent', to: 'properties#for_rent', as: 'for_rent'
+  # get '/account/properties', to: 'properties#my_properties'
+  # post '/agent/message', to: 'properties#email_agent', as: 'email_agent'
 
-  get '/dashboard', to: 'dashboard#index'
-  get '/profile/:id', to: 'dashboard#profile', as: 'profile'
+  # get '/dashboard', to: 'dashboard#index'
+  # get '/profile/:id', to: 'dashboard#profile', as: 'profile'
 
-  get '/accounts', to: 'admin#accounts', as: 'accounts'
+  # get '/accounts', to: 'admin#accounts', as: 'accounts'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+admin_account = Account.new(first_name: 'Francesca', last_name: 'Santoriello', email: 'milk@makelaars.com', company: 'Milk Makelaars', telephone: '+39 3394840509', admin: true )
+
+seed_property = Property.new(account_id: 1, name: 'Wooden Beauty', address: 'Oudezijds Voorburgwal 126', price: 2000000, rooms: 7, bedrooms: 4, bathrooms: 3)
