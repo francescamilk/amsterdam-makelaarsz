7dfd0819b5700f8f358b67652642b3288fb0e9c4
Francesca Santoriello <f.santoriello@hotmail.com>
2023-06-20 15:07:50 +0200

    per push

--- db/migrate/20210409092119_drop_old_aparments_table.rb
+++
@@ -1,5 +0,0 @@
-class DropOldAparmentsTable < ActiveRecord::Migration[6.0]
-  def change
-    drop_table :apartments
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -33,16 +33,6 @@ ActiveRecord::Schema.define(version: 2021_09_18_123134) do
     t.index ["reset_password_token"], name: "index_accounts_on_reset_password_token", unique: true
   end
 
-  create_table "action_text_rich_texts", force: :cascade do |t|
-    t.string "name", null: false
-    t.text "body"
-    t.string "record_type", null: false
-    t.bigint "record_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["record_type", "record_id", "name"], name: "index_action_text_rich_texts_uniqueness", unique: true
-  end
-
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -94,18 +84,6 @@ ActiveRecord::Schema.define(version: 2021_09_18_123134) do
     t.float "latitude"
     t.float "longitude"
     t.index ["account_id"], name: "index_properties_on_account_id"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
